c3d031890664456564ef91bf4e5d946fca3a560e
mateus <imw.desenv@gmail.com>
2023-09-24 17:21:26 -0300

    fix: change return type for pending subs

--- Univan.Application/Services/Subscriber/Queries/GetStudentPendingSubscriptions/GetStudentPendingSubscriptionsQuery.cs
+++ Univan.Application/Services/Subscriber/Queries/GetStudentPendingSubscriptions/GetStudentPendingSubscriptionsQuery.cs
@@ -4,7 +4,7 @@ using Univan.Application.Contracts.Subscription;
 
 namespace Univan.Application.Services.Subscriber.Queries.GetStudentPendingSubscriptions
 {
-    public class GetStudentPendingSubscriptionsQuery : IRequest<Result<StudentPendingSubscriptionsResult>>
+    public class GetStudentPendingSubscriptionsQuery : IRequest<Result<List<StudentPendingSubscriptionsResult>>>
     {
         public int StudentId { get; set; }
         public GetStudentPendingSubscriptionsQuery(int studentId)
--- Univan.Application/Services/Subscriber/Queries/GetStudentPendingSubscriptions/GetStudentPendingSubscriptionsQueryHandler.cs
+++ Univan.Application/Services/Subscriber/Queries/GetStudentPendingSubscriptions/GetStudentPendingSubscriptionsQueryHandler.cs
@@ -4,9 +4,9 @@ using Univan.Application.Contracts.Subscription;
 
 namespace Univan.Application.Services.Subscriber.Queries.GetStudentPendingSubscriptions
 {
-    public class GetStudentPendingSubscriptionsQueryHandler : IRequestHandler<GetStudentPendingSubscriptionsQuery, Result<StudentPendingSubscriptionsResult>>
+    public class GetStudentPendingSubscriptionsQueryHandler : IRequestHandler<GetStudentPendingSubscriptionsQuery, Result<List<StudentPendingSubscriptionsResult>>>
     {
-        public async Task<Result<StudentPendingSubscriptionsResult>> Handle(GetStudentPendingSubscriptionsQuery request, CancellationToken cancellationToken)
+        public async Task<Result<List<StudentPendingSubscriptionsResult>>> Handle(GetStudentPendingSubscriptionsQuery request, CancellationToken cancellationToken)
         {
             StudentPendingSubscriptionsResult pendingSubscriptions = null;
             //var pendingSubscriptions = _subscriptionRepository.GetStudentPendingSubscription(request.StudentId);
@@ -15,12 +15,15 @@ namespace Univan.Application.Services.Subscriber.Queries.GetStudentPendingSubscr
              *  select for subscription table by studentId and pending status. Lista de subscription
              */
 
-            var result = new StudentPendingSubscriptionsResult()
+            var result = new List<StudentPendingSubscriptionsResult>()
             {
-                DriverName = "Teste",
-                DriverPhone = "Teste",
-                ExpirationDay = 12,
-                MonthlyFee = 213.23M
+                new StudentPendingSubscriptionsResult()
+                {
+                    DriverName = "Teste",
+                    DriverPhone = "Teste",
+                    ExpirationDay = 12,
+                    MonthlyFee = 213.23M
+                }
             };
 
             return Result.Ok(result);
